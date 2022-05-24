28f4b4060c7b4498d75ec30f2153b7d901027402
monirul360 <monirulislam65361@gmail.com>
2022-05-25 00:51:08 +0600

    perts api create

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json());
 
+// mongodb connection 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.Us_name}:${process.env.DBPASS}@cluster0.chhfl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const pertsCollection = client.db("Computers").collection("perts");
+        app.post('/perts', async (req, res) => {
+            const perts = req.body;
+            const result = await pertsCollection.insertOne(perts);
+            res.send(result);
+        })
+    }
+    finally {
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Server open!')
 })
