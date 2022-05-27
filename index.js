fc7fd65680d8c9fa332d3931b066498ee29ed881
monirul360 <monirulislam65361@gmail.com>
2022-05-28 02:43:33 +0600

    profile update api create

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json());
 
+const stripe = require("stripe")('sk_test_51L2KrPBqppFXqmqSMlSHkIQUBu9sVzUvYZVsN45p8WachNPw0Rcer0aPQNcnGaOnuGPQ0RipdL2b2bexyqHMmpTY008WdeIEOV');
 // mongodb connection 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -38,6 +39,8 @@ async function run() {
         const reviewCollection = client.db("Computers").collection("review");
         const bookingCollection = client.db("Computers").collection("booking");
         const userCollection = client.db("Computers").collection("user");
+        const paymentCollection = client.db('Computers').collection('payments');
+        //   
         app.post('/perts', async (req, res) => {
             const perts = req.body;
             const result = await pertsCollection.insertOne(perts);
@@ -85,6 +88,28 @@ async function run() {
                 return res.status(403).send({ message: 'Forbidden access' })
             }
         })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const users = await userCollection.findOne({ email: email });
+            res.send(users);
+        })
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updatedDoc,
+                options
+            );
+            res.send(result);
+        })
+
+        // 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -125,6 +150,38 @@ async function run() {
             }
         })
 
+        app.post('/create-payment-intent', async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
+        app.put('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const result = await paymentCollection.insertOne(payment);
+            const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+            res.send(updatedBooking);
+        })
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.findOne(query);
+            res.send(result);
+        })
     }
 
     finally {
